1306c762caca69d8c42ef378676e56c9e704a1a6
Mark M <chtozamm@gmail.com>
2024-12-13 13:28:57 +0300

    Complete Day 13

---
+++ day_13/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+)
+
+type testCase struct {
+	input    []byte
+	expected int
+}
+
+func TestPartOne(t *testing.T) {
+	t.Run("first", func(t *testing.T) {
+		input := []byte(`Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279`)
+		expected := 480
+		if got := partOne(input); got != expected {
+			t.Errorf("got %v, expected %v", got, expected)
+		}
+	})
+}
---
+++ day_13/part1.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func partOne(input []byte) int {
+	tokens := 0
+	machines := strings.Split(string(input), "\n\n")
+
+	type Coordinates struct {
+		X int
+		Y int
+	}
+
+	type Machine struct {
+		ButtonA Coordinates
+		ButtonB Coordinates
+		Prize   Coordinates
+	}
+
+	parseCoordinates := func(line string) Coordinates {
+		re := regexp.MustCompile(`\d+`)
+		matches := re.FindAllString(line, -1)
+		x, _ := strconv.Atoi(matches[0])
+		y, _ := strconv.Atoi(matches[1])
+		return Coordinates{x, y}
+	}
+
+	for _, machine := range machines {
+		components := strings.Split(machine, "\n")
+		m := &Machine{
+			ButtonA: parseCoordinates(components[0]),
+			ButtonB: parseCoordinates(components[1]),
+			Prize:   parseCoordinates(components[2]),
+		}
+
+		/*
+		   Assume that:
+		   a = ButtonA, b = ButtonB, prize = Prize, s = countA, t = countB
+
+		   System of linear equations:
+		   aX * s + bX * t = prizeX
+		   aY * s + bY * t = prizeY
+
+		   Formulas to find counts:
+		   s = (prizeX * bY) / (aX * bY - aY * bX)
+		   t = (prize - aX * s) / bX
+		*/
+
+		countA := float64(m.Prize.X*m.ButtonB.Y-m.Prize.Y*m.ButtonB.X) / float64(m.ButtonA.X*m.ButtonB.Y-m.ButtonA.Y*m.ButtonB.X)
+		countB := float64(float64(m.Prize.X)-float64(m.ButtonA.X)*countA) / float64(m.ButtonB.X)
+
+		// Check if counts are integers
+		if countA == math.Floor(countA) && countB == math.Floor(countB) {
+			// Each button must be pressed no more than 100 times
+			if countA <= 100 && countB <= 100 {
+				tokens += int(countA)*3 + int(countB)
+			}
+		}
+	}
+
+	return tokens
+}
